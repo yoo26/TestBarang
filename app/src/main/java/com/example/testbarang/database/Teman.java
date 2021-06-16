703fd7b218359421a4b4ec503e62bf74626bba6b
Yoo <79823445+asthey@users.noreply.github.com>
2021-06-16 17:20:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testbarang/database/Teman.java
@@ -0,0 +1,40 @@
+package com.example.testbarang.database;
+
+import java.io.Serializable;
+
+public class Teman implements Serializable {
+    String nama;
+    String telpon;
+
+    public Teman() {
+    }
+
+    public Teman(String nama, String telpon) {
+        this.nama = nama;
+        this.telpon = telpon;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTelpon() {
+        return telpon;
+    }
+
+    public void setTelpon(String telpon) {
+        this.telpon = telpon;
+    }
+
+    @Override
+    public String toString() {
+        return "Teman{" +
+                "nama='" + nama + '\'' +
+                ", telpon='" + telpon + '\'' +
+                '}';
+    }
+}
